67040bc3a0dcc9d6959a53854ca8681dd0fa317c
admin <admin@localhost.localdomain>
2019-11-24 17:24:41 +0700

    added start arguments for server.js file, added start and end lines + ranges

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
+var data;
+var stat;
+
+if (process.argv.length == 6 && process.argv[2] == "--data-path" && process.argv[4] == "--stat-path") {
+	data = process.argv[3]
+	stat = process.argv[5]
+}else if (process.argv.length == 6 && process.argv[4] == "--data-path" && process.argv[2] == "--stat-path"){
+	data = process.argv[5]
+	stat = process.argv[3]	
+}else {
+    console.log("Usage: node your_prog.js --data-path path_to-data --stat-path path_to-stat");
+    process.exit(-1);
+}
+ 
+console.log('Data: ' + data);
+console.log('Stat: ' + stat);
+
 var http = require("http");
 var fs = require("fs");
 
 http.createServer(function (request, response) {
-    console.log(request.url);
+    console.log("Request: " + request.url);
     var filePath = request.url.substr(1);
     if(filePath === "")
         filePath = "index.html";
-    console.log(filePath);
+    //console.log(filePath);
+    if(request.url === "/stat") {
+		console.log('Stat to send: ' + stat);
+		response.end(stat);
+		return;
+	}else if(request.url === "/data") {
+		console.log('Data to send: ' + data);
+		response.end(data);
+		return;
+	}
+	console.log("Send: " + filePath);
     fs.readFile(filePath, function (error, data) {
         response.end(data);
     });
+    /*http.get("/stat", function(request, response){
+		response.send(stat);
+	});*/
     return;
 }).listen(3000, function () {
     console.log("hello");
